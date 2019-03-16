fda18e9c657703456707189cbebedaafbe1b7c6b
dipak140 <dipak.work.14@gmail.com>
2019-03-16 11:02:25 +0530

    Update Prime Experiment.py

--- Prime Experiment.py
+++ Prime Experiment.py
@@ -5,23 +5,19 @@ Created on Sat Mar 16 10:46:11 2019
 @author: Dipak Kumar
 """
 n = 0
-
-def print_factors(x):
+def primecount(x):
    count = 0
-   # This function takes a number and prints the factors
-   #print("The factors of",x,"are:")
    for j in range(1, x + 1):
        if x % j == 0:
            count += 1
    return count
-
 i = 1
 while i==1:
     P = (n**2) + (n + 41)
-    count = print_factors(P)
+    count = primecount(P)
     if count == 2:
-        print("{0} is a prime number".format(P))
+        print("{0} is a prime number for n = {1}".format(P,n))
     if count > 2:
-        print("{0} is not a prime number for n={1}".format(P,n))
+        print("{0} is not a prime number for n = {1}".format(P,n))
         break
     n = n+1
